88e26a1178e1693fb68b4e62cc1e2da10fa22cc4
smitshah1999@gmail.com <smitshah1999@gmail.com>
2020-04-27 15:49:39 +0530

    adds code for reading disease treatments and descriptions from folder

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class MedicalExpert(KnowledgeEngine):
         It acts like a constructor to this class.
         """        
         yield Fact(findDisease = 'true')
-        print("Hi! I am Mr.Expert.You can get yourself diagnosed here free of cost!I will ask you 10 questions")
+        print("Hi! I am Mr.Expert.\n\nYou can get yourself diagnosed here free of cost!\nI will ask you 10 questions.\n\n")
         
 
     @Rule(Fact(findDisease = 'true'),NOT(Fact(name=W())),salience = 1000)
@@ -48,24 +48,61 @@ class MedicalExpert(KnowledgeEngine):
         fainting = input("\nDo you faint occasionally?\nPlease type Yes/No\n")
         self.declare(Fact(fainting = fainting.strip().lower()))
 
+
+   
+#  sunken eyes, low body temperature,
+# restlessness, sore throat, fever
+
+    @Rule(Fact(findDisease='true'), NOT (Fact(fatigue = W())),salience = 970)
+    def hasFatigue(self):
+        fatigue = input("\nDo you experience fatigue occasionally?\nPlease type Yes/No\n")
+        self.declare(Fact(fatigue = fatigue.strip().lower()))
+
+    @Rule(Fact(findDisease='true'), NOT (Fact(headache = W())),salience = 965)
+    def hasHeadache(self):
+        headache = input("\nDo you experience headaches?\nPlease type Yes/No\n")
+        self.declare(Fact(headache = headache.strip().lower()))
+    
+    @Rule(Fact(findDisease='true'), (Fact(headache = 'yes')),salience = 960)
+    def hasSevereheadache(self):
+        severe_headache = input("\nIs it too severe?\nPlease type Yes/No\n")
+        self.declare(Fact(severe_headache = severe_headache.strip().lower()))
+
+    @Rule(Fact(findDisease='true'), NOT (Fact(back_pain = W())),salience = 955)
+    def hasbackPain(self):
+        back_pain = input("\nDo you experience back pains?\nPlease type Yes/No\n")
+        self.declare(Fact(back_pain = back_pain.strip().lower()))
+    
+
+
+
+
+
+
+
     @Rule(Fact(findDisease='true'), Fact(severe_chestPain = 'yes'), Fact(cough='no'))
     def disease_0(self):
-        self.declare(Fact(disease = 'covid'))
+        self.declare(Fact(disease = 'Covid'))
+
+
 
     @Rule(Fact(findDisease = 'true'),Fact(disease = MATCH.disease),salience = -1)
     def getDisease(self, disease):
         print('The most probable illness you are suffering from is:',disease)
         print('\n\n')
-        print('Some info about the disease : ')
+        print('Some info about the disease:\n')
         
+        f = open("disease/disease_descriptions/" + disease + ".txt", "r")
+        print(f.read())
+
+        print('\n\nNo need to worry',self.username,'. We even have some preventive measures for you!\n')
+        f = open("disease/disease_treatments/" + disease + ".txt", "r")
+        print(f.read())
     # @Rule(Fact(findDisease = 'true'),
     # Fact(name=MATCH.name))
     # def greet(self, name):
     #     print("Hi!",name, "How is the weather?")
-if __name__ == "__main__": 
-    
-
-
+if __name__ == "__main__":
     engine = MedicalExpert()
     engine.reset()
     engine.run()
